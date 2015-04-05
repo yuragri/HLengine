edf40c90da290ccf6f25acaae393c3ad7bc85513
ArtArtArtArt <dartem@meta.ua>
2015-04-05 16:46:54 +0300

    A lot of changes
    A lot of them

--- Common/d3dApp.cpp
+++ Common/d3dApp.cpp
@@ -311,15 +311,15 @@ LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_LBUTTONDOWN:
 	case WM_MBUTTONDOWN:
 	case WM_RBUTTONDOWN:
-		OnMouseDown(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		OnMouseDown(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), mScreenViewport);
 		return 0;
 	case WM_LBUTTONUP:
 	case WM_MBUTTONUP:
 	case WM_RBUTTONUP:
-		OnMouseUp(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		OnMouseUp(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), mScreenViewport);
 		return 0;
 	case WM_MOUSEMOVE:
-		OnMouseMove(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		OnMouseMove(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), mScreenViewport);
 		return 0;
 	case WM_KEYDOWN:
 		OnKeyDown(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
